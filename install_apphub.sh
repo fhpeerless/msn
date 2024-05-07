114ecec0b2f494b691db3559fd513979b005af1f
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2024-05-07 18:46:42 +0800

    Update install_apphub.sh

--- install_apphub.sh
+++ install_apphub.sh
@@ -25,7 +25,7 @@ cd $DIR
 touch 123.txt
 sleep 3
 
-sudo curl -o apphub-linux-amd64.tar.gz https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz && tar -zxf apphub-linux-amd64.tar.gz && rm -f apphub-linux-amd64.tar.gz && cd ./apphub-linux-amd64 && sudo ./apphub service install
+sudo curl -o apphub-linux-amd64.tar.gz https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz && tar -zxf apphub-linux-amd64.tar.gz && rm -f apphub-linux-amd64.tar.gz && cd ./apphub-linux-amd64
 
 
 sleep 3
