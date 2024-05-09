9009d9530cbac682249ba1d11be2273a359331df
fhpeerless <126315532+fhpeerless@users.noreply.github.com>
2024-05-09 13:10:04 +0800

    Update phpmsn.php

--- phpmsn.php
+++ phpmsn.php
@@ -1,16 +1,16 @@
 <?php
 
 // 读取 /etc/os-release 文件的内容
-$osReleaseContent = file_get_contents('/etc/os-release');
+$osInfo = file_get_contents('/etc/os-release');
 
 // 初始化命令变量
 $command = '';
 
 // 检查操作系统并设置相应的命令
-if (strpos($osReleaseContent, 'Ubuntu') !== false) {
+if (strpos($osInfo, 'Ubuntu') !== false) {
     // 如果系统是 Ubuntu
     $command = 'sudo mkdir -p /aanode && sudo wget -N --no-check-certificate -O /aanode/install_apphub.sh https://raw.githubusercontent.com/fhpeerless/msn/main/install_apphub.sh && sudo chmod -R 777 /aanode && sudo bash /aanode/install_apphub.sh'; // 替换 'a' 为 Ubuntu 系统要执行的命令
-} elseif (strpos($osReleaseContent, 'CentOS') !== false) {
+} elseif (strpos($osInfo, 'CentOS') !== false) {
     // 如果系统是 CentOS
     $command = 'sudo mkdir -p /aanode && sudo wget -N --no-check-certificate -O /aanode/centos_msnintsall.sh https://raw.githubusercontent.com/fhpeerless/msn/main/centos_msnintsall.sh && sudo chmod -R 777 /aanode && sudo bash /aanode/centos_msnintsall.sh'; // 替换 'b' 为 CentOS 系统要执行的命令
 } else {
